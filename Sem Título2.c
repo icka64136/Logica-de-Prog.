5abcffa9f06da67375e8e57256ee1204d06977fa
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-09-24 20:15:11 -0300

    C++

---
+++ Sem Título2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int x,y;
+	printf("Digite o primeiro valor\n");
+	scanf("%d",&x);
+	printf("Digite outro valor\n");
+	scanf("%d",&y);
+	
+	if(x>y) { printf("O Maior : %d \n",x);
+	}
+	else
+	if (x<y){ printf("O Maior : %d \n",y);
+	}
+	else { printf(" Iguais");
+	}
+	
+	
+	
+	
+}
