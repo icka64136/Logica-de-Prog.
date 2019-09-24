b0220efd7b1a61036bea6c0640bbe0b8a73ea30e
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-09-24 20:36:36 -0300

    x4

---
+++ Programa x4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+
+int x,y;
+printf("Digite dois valores \n");
+scanf("%d""%d",&x,&y);
+//printf("Digite mais um valor \n");
+//scanf("%d",&y);
+
+printf("%d*4= %d , %d*3= %d",x,x*4 ,y,y*3);
+}
