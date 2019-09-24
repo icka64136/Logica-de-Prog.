6c636df821ba43ca167769a86f7e7d8d872dcb4e
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-09-24 20:14:37 -0300

    C++ Programa

---
+++ Sem Título1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int x;
+	printf("Digite a Sua Idade\n");
+	scanf("%d",&x);
+	if(x>=18)
+	{ printf("Maior idade\n");
+	}
+	else { printf("Menor Idade\n");
+	}
+	
+	
+	
+	
+}
