f24383435befee1869d6bca679c45bd492cc73c7
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-09-24 21:37:50 -0300

    Ordem Crescente

---
+++ Ordem Crescente..c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{ int y,z;
+	printf("Digite o primeiro valor \n");
+	scanf("%d",&y);
+	
+	printf("Digite o segundo valor \n");
+	scanf("%d",&z);
+	
+		if (y>z)	
+		{	
+			printf(" Ordem Crescente:%d , %d \n",z,y);
+	}
+				else 	
+				{
+					printf("Ordem Crescente :%d , %d \n ",y,z); 
+	}
+}
+	
