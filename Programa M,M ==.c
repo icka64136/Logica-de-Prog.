acfbd21d6ada9e6241cce6c6a755f07054830ab0
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-09-24 21:07:55 -0300

    mm==

---
+++ Programa M,M ==.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{ int x;
+	printf("Digite um valor \n");
+	scanf("%d",&x);
+		if  (x>10) { 		
+			printf(" Numero digitado Maior que : 10 \n");}
+				
+				else  
+				
+					if (x<10) { printf(" Numero digitado Menor que : 10 \n");}
+				
+					else { printf(" Numero digitado sao iguais \n");	}
+					
+
+
+
+
+
+
+
+
+
+
+}
+	
+	
+
