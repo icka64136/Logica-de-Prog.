592140f6d253d86738b288d407afb2d53a09b118
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-10-01 22:48:00 -0300

    Questão Extra 01/10/19

---
+++ Programa Quadrado ou Cubo, Maior Par , Menor Negativo.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+main()
+{
+	int x,y,z;
+	printf("Digite um Valor. \n ");
+	
+	scanf("%d",&x);
+			
+	printf("Digite mais um Valor. \n");
+	
+	scanf("%d",&y);
+	
+	if( x > y)
+	{
+		x = x;
+		y = y;
+	}
+	
+		else
+		{
+			z=x;
+			x=y;
+			y=z;
+			
+		}	
+		
+			if (x == y)
+			{
+				printf("O Quadrado dos valores sao : %d^2 = %d \n ", x,x*y);
+			}
+								
+				else
+				{
+					printf("O Cubo dos valores sao : %d ^ 3 = %d , %d ^ 3 = %d \n ", x,x*x*x, y,y*y*y);
+					
+					if ( x%2==0 )
+						
+					{
+						printf(" Maior : %d Par \n",x);					
+						
+						if (y<0)						
+							{
+							printf("Menor : %d Negativo",y);							
+							}
+							
+					}
+									
+				 else 
+				 if (y<0)
+				 {
+				 	printf("Menor : %d Negativo",y);
+				 }	
+				}
+				
+				
+					
+	/* Elabore um algoritmo em C que leia 2 valores
+	 inteiros e mostre o quadrado dos números 
+	se forem iguais e o cubo se forem diferentes. Se forem diferentes diga
+	 se o maior é par e se o menor é negativo.*/
+}
