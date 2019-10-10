f0178200b0486b80cc29db9497cc58f5b0a0d993
icka64136 <54457262+icka64136@users.noreply.github.com>
2019-10-10 20:29:35 -0300

    Add files via upload

---
+++ Switch Exercicio 1 de 3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	int m;
+	
+	printf("Digite um Valor entre 1 e 12 \n");
+	scanf("%d",&m);
+	
+	switch(m){
+		case 1: printf ("Janeiro \n");break;
+		case 2: printf ("Fevereiro \n");break;
+		case 3: printf ("Marco \n");break;
+		case 4: printf ("Abril \n");break;
+		case 5: printf ("Maio \n");break;
+		case 6: printf ("Junho \n");break;
+		case 7: printf ("Julho \n");break;
+		case 8: printf ("Agosto \n");break;
+		case 9: printf ("Setembro \n");break;
+		case 10: printf ("Outubro \n");break;
+		case 11: printf ("Novembro \n");break;
+		case 12: printf ("Dezembro \n");break;
+		default : printf("Numero Invalido");
+	}	
+}
